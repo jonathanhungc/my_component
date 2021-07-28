abcb27b537fb4588c7ccca55c42195910bc64052
jonathanhungc <jonathanhung2001@gmail.com>
2021-07-27 19:33:34 -0700

    Update my_component.js

--- javascripts/discourse/api-initializers/my_component.js
+++ javascripts/discourse/api-initializers/my_component.js
@@ -4,14 +4,14 @@ import { ajax } from 'discourse/lib/ajax';
 export default apiInitializer("0.11.1", api => {
   console.log("hello world from api initializer!"); 
 
-  // api.registerConnectorClass('below-site-header', 'greeting-banner', {
-  //   setupComponents(args, component) {
-  //     ajax(`/u/${username}.json`).then((badgeData) => {
-  //       let user = badgeData.user;
-  //       component.set('user', user)
-  //     });
-  //   },
-  // });
+  api.registerConnectorClass('below-site-header', 'greeting-banner', {
+    setupComponents(args, component) {
+      ajax(`/u/${username}.json`).then((badgeData) => {
+        let user = badgeData.user;
+        component.set('user', user)
+      });
+    },
+  });
 
-  // console.log(user);
+  console.log(user);
 });
