373ea3f4deafd205914888732a926d68a82bb741
jonathanhungc <jonathanhung2001@gmail.com>
2021-07-27 19:06:13 -0700

    Update my_component.js

--- javascripts/discourse/api-initializers/my_component.js
+++ javascripts/discourse/api-initializers/my_component.js
@@ -2,13 +2,14 @@ import { apiInitializer } from "discourse/lib/api";
 import { ajax } from 'discourse/lib/ajax';
 
 const currentUser = Discourse.User.current();
+var username = currentUser.username;
 
 export default apiInitializer("0.11.1", api => {
   console.log("hello world from api initializer!"); 
 
   api.registerConnectorClass('below-site-header', 'my_component', {
     setupComponents(args, component) {
-      ajax('/u/currentUser.json').then((badgeData) => {
+      ajax(`/u/${currentUser}.json`).then((badgeData) => {
         let user = badgeData.user;
         component.set('user', user)
       });
