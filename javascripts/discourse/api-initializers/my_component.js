d23fae52981daa80270513f8103f852ea8cba36b
jonathanhungc <jonathanhung2001@gmail.com>
2021-07-27 19:02:12 -0700

    Added API request

--- javascripts/discourse/api-initializers/my_component.js
+++ javascripts/discourse/api-initializers/my_component.js
@@ -1,6 +1,20 @@
 import { apiInitializer } from "discourse/lib/api";
 import { ajax } from 'discourse/lib/ajax';
 
+const currentUser = Discourse.User.current();
+
 export default apiInitializer("0.11.1", api => {
-  console.log("hello world from api initializer!");
+  console.log("hello world from api initializer!"); 
+
+  api.registerConnectorClass('below-site-header', 'my_component', {
+    setupComponents(args, component) {
+      ajax('/u/currentUser.json').then((badgeData) => {
+        let user = badgeData.user;
+        component.set('user', user)
+      });
+    },
+  });
+
+  console.log("Loaded connector class."); 
+
 });
