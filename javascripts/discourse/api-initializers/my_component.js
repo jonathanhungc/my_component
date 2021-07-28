d296f42abdeb4e160240a54df25b610d1123636a
jonathanhungc <jonathanhung2001@gmail.com>
2021-07-27 19:16:47 -0700

    a

--- javascripts/discourse/api-initializers/my_component.js
+++ javascripts/discourse/api-initializers/my_component.js
@@ -1,29 +1,20 @@
 import { apiInitializer } from "discourse/lib/api";
 import { ajax } from 'discourse/lib/ajax';
 
-// const currentUser = Discourse.User.current();
-// var user = currentUser.username;
-
-// export default apiInitializer("0.11.1", api => {
-//   console.log("hello world from api initializer!"); 
-
-//   api.registerConnectorClass('below-site-header', 'greeting-banner', {
-//     setupComponents(args, component) {
-//       ajax(`/u/${user}.json`).then((badgeData) => {
-//         let user = badgeData.user;
-//         component.set('user', user)
-//       });
-//     },
-//   });
-// });
+const currentUser = Discourse.User.current();
+var user = currentUser.username;
 
 export default apiInitializer("0.11.1", api => {
-  api.registerConnectorClass('below-site-header', 'greeting-banner', {
-    setupComponent(args, component) {
-      ajax('/badges.json').then((badgeData) => {
-        let badges = badgeData.badges;
-        component.set('badges', badges);
-      });
-    },
-  });
+  console.log("hello world from api initializer!"); 
+
+  // api.registerConnectorClass('below-site-header', 'greeting-banner', {
+  //   setupComponents(args, component) {
+  //     ajax(`/u/${user}.json`).then((badgeData) => {
+  //       let user = badgeData.user;
+  //       component.set('user', user)
+  //     });
+  //   },
+  // });
+
+  console.log(user)
 });
