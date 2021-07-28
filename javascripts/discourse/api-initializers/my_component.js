639f72c01bc26294a2c4e56d00cf7374a6bebc99
jonathanhungc <jonathanhung2001@gmail.com>
2021-07-27 22:31:00 -0700

    Update my_component.js

--- javascripts/discourse/api-initializers/my_component.js
+++ javascripts/discourse/api-initializers/my_component.js
@@ -1,5 +1,4 @@
 import { apiInitializer } from "discourse/lib/api";
-import { ajax } from 'discourse/lib/ajax';
 
 export default apiInitializer("0.11.1", api => {
   console.log("hello world from api initializer!"); 
