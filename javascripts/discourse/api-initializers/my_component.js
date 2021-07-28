c5fd8e387049c9c9b4160efe9f41a6c206759423
jonathanhungc <jonathanhung2001@gmail.com>
2021-07-27 19:19:13 -0700

    Update my_component.js

--- javascripts/discourse/api-initializers/my_component.js
+++ javascripts/discourse/api-initializers/my_component.js
@@ -1,8 +1,8 @@
 import { apiInitializer } from "discourse/lib/api";
 import { ajax } from 'discourse/lib/ajax';
 
-const currentUser = Discourse.User.current();
-var user = currentUser.username;
+// const currentUser = Discourse.User.current();
+// var user = currentUser.username;
 
 export default apiInitializer("0.11.1", api => {
   console.log("hello world from api initializer!"); 
@@ -16,5 +16,5 @@ export default apiInitializer("0.11.1", api => {
   //   },
   // });
 
-  console.log(user);
+  // console.log(user);
 });
