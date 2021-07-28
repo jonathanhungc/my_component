5d6dc01aa64a335c2c5647be728826c16e9e03d5
jonathanhungc <jonathanhung2001@gmail.com>
2021-07-27 19:42:47 -0700

    Update my_component.js

--- javascripts/discourse/api-initializers/my_component.js
+++ javascripts/discourse/api-initializers/my_component.js
@@ -1,17 +1,28 @@
 import { apiInitializer } from "discourse/lib/api";
 import { ajax } from 'discourse/lib/ajax';
 
-export default apiInitializer("0.11.1", api => {
-  console.log("hello world from api initializer!"); 
+// export default apiInitializer("0.11.1", api => {
+//   console.log("hello world from api initializer!"); 
 
-  api.registerConnectorClass('below-site-header', 'greeting-banner', {
-    setupComponents(args, component) {
-      ajax('/u/jonathanh.json').then((badgeData) => {
-        let user = badgeData.user;
-        component.set('user', user)
+//   api.registerConnectorClass('below-site-header', 'greeting-banner', {
+//     setupComponents(args, component) {
+//       ajax('/u/jonathanh.json').then((badgeData) => {
+//         let user = badgeData.user;
+//         component.set('user', user)
+//       });
+//     },
+//   });
+
+//   console.log(user);
+// });
+
+export default apiInitializer("0.11.1", api => {
+  api.registerConnectorClass('below-site-header', 'notification-banner', {
+    setupComponent(args, component) {
+      ajax('/badges.json').then((badgeData) => {
+        let badges = badgeData.badges;
+        component.set('badges', badges);
       });
     },
   });
-
-  console.log(user);
 });
